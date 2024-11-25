d7d842893f70574df4c559c155c2dc50d406fb72
Gonzalo-LS <134894135+Gonzalo-LS@users.noreply.github.com>
2024-11-25 18:23:11 +0100

    Update server.js

--- gtareas/server.js
+++ gtareas/server.js
@@ -192,6 +192,7 @@ app.delete('/tareas/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en el puerto 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => { 
+  console.log(`Servidor corriendo en el puerto ${port}`);
 });
