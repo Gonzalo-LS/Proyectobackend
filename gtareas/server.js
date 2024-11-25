2e494d376eec250beca06e7b3cca4c59901e03ea
Gonzalo-LS <134894135+Gonzalo-LS@users.noreply.github.com>
2024-11-25 18:40:03 +0100

    Update server.js

--- gtareas/server.js
+++ gtareas/server.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://gestortareasfoc.vercel.app',
+}));
 
 
 const db = new Pool({
